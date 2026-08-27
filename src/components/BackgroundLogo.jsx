acc1973549d10211e44dcef78e273b684ed7feb4
DEC <75128626+EmiliaCristina@users.noreply.github.com>
2026-08-27 13:41:56 +0200

    Update BackgroundLogo.jsx

--- src/components/BackgroundLogo.jsx
+++ src/components/BackgroundLogo.jsx
@@ -4,20 +4,19 @@ import * as THREE from 'three';
 import { publicUrl } from '../utils/publicUrl';
 
 export function BackgroundLogo(props) {
-  // Carica l'immagine dalla cartella public/
   const texture = useTexture(publicUrl('/Asset 4.png'));
 
   return (
     <mesh
-      position={[0, 0, -2]} // Posizionato dietro al modello 3D
-      scale={[3.5, 3.5, 1]}  // Dimensione del logo
+      position={[0, 0, -2]}
+      scale={[3.5, 3.5, 1]}
       {...props}
     >
       <planeGeometry args={[1, 1]} />
       <meshBasicMaterial
         map={texture}
         transparent={true}
-        opacity={1} // Regola l'opacità (es. 0.15 watermark, 0.8 visibile, 1.0 pieno)
+        opacity={1}
         depthWrite={false}
         side={THREE.DoubleSide}
       />
